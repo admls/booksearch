4348b6b46271e05a2e8ef7afcb5d7903929fafd0
admls <adam.the.sherwood@gmail.com>
2018-12-18 15:22:58 +0000

    created index.css. Changed booklist format to be easily edited.

--- booksearch.js
+++ booksearch.js
@@ -11,7 +11,7 @@ const booklist = {
             maxPrice: maxPrice
         });
     },
-    confirmEditBook: function(position, title, author, maxPrice) {
+    editBook: function(position, title, author, maxPrice) {
         this.books[position].title = title;
         this.books[position].author = author;
         this.books[position].maxPrice = maxPrice;
@@ -35,48 +35,20 @@ const handlers = {
         addBookTitle.focus();
     },
     editBook: function(position) {
-        let bookLi = document.getElementById(position);
-        const editingBookLi = document.createElement('li');
-        editingBookLi.id = position;
-
-        const editingTitle = document.createElement('input');
-        editingTitle.value = bookLi.querySelector(".title").textContent;
-        editingTitle.className = "title";
-
-        const editingAuthor = document.createElement('input');
-        editingAuthor.value = bookLi.querySelector(".author").textContent;
-        editingAuthor.className = "author";
-
-        const editingMaxPrice = document.createElement('input');
-        editingMaxPrice.className = "maxPrice";
-        editingMaxPrice.value = bookLi.querySelector(".maxPrice").textContent;
-
-        editingBookLi.appendChild(editingTitle);
-        editingBookLi.appendChild(editingAuthor);
-        editingBookLi.appendChild(editingMaxPrice);
-
-        editingBookLi.appendChild(view.createConfirmEditButton());
-        editingBookLi.appendChild(view.createCancelEditButton());
-        editingBookLi.appendChild(view.createDeleteButton());
-        bookLi.replaceWith(editingBookLi);
-    },
-    cancelEditBook: function() {
-        view.displayBooklist();
-    },
-    confirmEditBook: function(position) {
-        const editingBookLi = document.getElementById(position);
-        const bookTitle = editingBookLi.querySelector(".title").value;
-        const bookAuthor = editingBookLi.querySelector(".author").value;
-        const bookMaxPrice = editingBookLi.querySelector(".maxPrice").value;
-        console.log(bookTitle);
-        console.log(bookAuthor);
-        console.log(bookMaxPrice);
-        booklist.confirmEditBook(position, bookTitle, bookAuthor, bookMaxPrice);
-        view.displayBooklist();
+        const bookLi = document.getElementById(position);
+        const bookTitle = bookLi.querySelector(".title").value;
+        const bookAuthor = bookLi.querySelector(".author").value;
+        const bookMaxPrice = bookLi.querySelector(".maxPrice").value;
+        booklist.editBook(position, bookTitle, bookAuthor, bookMaxPrice);
     },
     deleteBook: function(position) {
-        booklist.deleteBook(position);
-        view.displayBooklist();
+        const bookLi = document.getElementById(position);
+        bookLi.style.animationPlayState = "running";
+        bookLi.addEventListener("animationend", () => {
+            console.log(bookLi.style.height);
+            booklist.deleteBook(parseInt(position));
+            view.displayBooklist();
+        })
     }
   };
 
@@ -87,65 +59,51 @@ var view = {
         booklistUl.innerHTML = "";
         booklist.books.forEach(function(book, position) {
             const bookLi = document.createElement('li');
-            const titleSpan = document.createElement('span');
-            const authorSpan = document.createElement('span');
-            const maxPriceSpan = document.createElement('span');
+            const titleInput = document.createElement('input');
+            const authorInput = document.createElement('input');
+            const maxPriceInput = document.createElement('input');
 
-            titleSpan.textContent = book.title;
-            titleSpan.className = "title";
-            authorSpan.textContent = book.author;
-            authorSpan.className = "author";
-            maxPriceSpan.textContent = book.maxPrice;
-            maxPriceSpan.className = "maxPrice";
+            titleInput.value = book.title;
+            titleInput.className = "title";
+
+            authorInput.value = book.author;
+            authorInput.className = "author";
+
+            maxPriceInput.value = book.maxPrice;
+            maxPriceInput.className = "maxPrice";
+
             bookLi.id = position;
-            bookLi.appendChild(titleSpan);
-            bookLi.appendChild(authorSpan);
-            bookLi.appendChild(maxPriceSpan);
-            bookLi.appendChild(this.createEditButton());
+            bookLi.appendChild(titleInput);
+            bookLi.appendChild(authorInput);
+            bookLi.appendChild(maxPriceInput);
             bookLi.appendChild(this.createDeleteButton());
             booklistUl.appendChild(bookLi);
         }, this);
     },
-    createEditButton: function() {
-        const editButton = document.createElement("button");
-        editButton.textContent = "Edit";
-        editButton.className = "editButton";
-        return editButton
-    },
-    createConfirmEditButton: function() {
-        const confirmEditButton = document.createElement("button");
-        confirmEditButton.textContent = "Confirm";
-        confirmEditButton.className = "confirmEditButton";
-        return confirmEditButton;
-    },
-    createCancelEditButton: function() {
-        const cancelEditButton = document.createElement("button");
-        cancelEditButton.textContent = "Cancel";
-        cancelEditButton.className = "cancelEditButton";
-        return cancelEditButton;
-    },
     createDeleteButton: function() {
         const deleteButton = document.createElement("button");
-        deleteButton.textContent = "Delete";
+        deleteButton.innerHTML = "&times;";
         deleteButton.className = "deleteButton";
         return deleteButton;
     },
     setUpEventListeners: function() {
         const booklistUl = document.querySelector("ul");
-
         booklistUl.addEventListener("click", function(event) {
             const elementClicked = event.target;
-            console.log(elementClicked);
             if (elementClicked.className === "deleteButton") {
-                handlers.deleteBook(parseInt(elementClicked.parentNode.id));
-            } else if (elementClicked.className === "editButton") {
-                handlers.editBook(parseInt(elementClicked.parentNode.id));
-            } else if (elementClicked.className === "confirmEditButton") {
-                handlers.confirmEditBook(parseInt(elementClicked.parentNode.id));
-            } else if (elementClicked.className === "cancelEditButton") {
-                handlers.cancelEditBook();
+                debugger;
+                handlers.deleteBook(elementClicked.parentNode.id);
             };
         });
+
+        document.querySelectorAll("ul.booklist input").forEach(function(bookField) {
+            bookField.addEventListener("keyup", () => {
+                handlers.editBook(parseInt(bookField.parentNode.id));
+            });
+            bookField.addEventListener("blur", () => {
+                this.displayBooklist();
+            })
+        }, this);
 
         document.querySelectorAll(".getResults").forEach(function(button) {
             button.addEventListener("click", function() {
